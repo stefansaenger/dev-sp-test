087e17f3b4b2d3d16252c090a65cc804e316b416
Stefan Sänger <stefan.saenger@vnc.biz>
2018-04-25 13:05:38 +0200

    adjusted config to work with static idp

--- config/config.js
+++ config/config.js
@@ -9,9 +9,9 @@ module.exports = {
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
         logoutCallbackUrl: '/logout/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'http://localhost:4000/saml2/idp/SSOService.php',
-        logoutUrl: process.env.SAML_ENTRY_POINT || 'http://localhost:4000/saml2/idp/SingleLogoutService.php',
-        issuer: 'local-dev-sp',
+        entryPoint: process.env.SAML_ENTRY_POINT || 'https://redminedevidp.vnc.biz/simplesaml/saml2/idp/SSOService.php',
+        logoutUrl: process.env.SAML_ENTRY_POINT || 'https://redminedevidp.vnc.biz/simplesaml/saml2/idp/SingleLogoutService.php',
+        issuer: 'saml-commander-dev',
         cert: process.env.SAML_CERT || null
       }
     }
