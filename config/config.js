6a7cad2c5b558cbc35f240cacd2e0bc8d7fabf79
Stefan Saenger <stefan.saenger@vnc.biz>
2025-08-12 09:01:30 +0200

    fix: update basics without passport-saml

--- config/config.js
+++ config/config.js
@@ -9,10 +9,11 @@ module.exports = {
       saml: {
         path: process.env.SAML_PATH || '/login/callback',
         logoutCallbackUrl: '/logout/callback',
-        entryPoint: process.env.SAML_ENTRY_POINT || 'https://redminedevidp.vnc.biz/simplesaml/saml2/idp/SSOService.php',
-        logoutUrl: process.env.SAML_ENTRY_POINT || 'https://redminedevidp.vnc.biz/simplesaml/saml2/idp/SingleLogoutService.php',
-        issuer: 'saml-commander-dev',
-        cert: process.env.SAML_CERT || null
+        entryPoint: process.env.SAML_ENTRY_POINT || 'https://vncidp.dev.vnc.de/saml2/idp/SSOService.php',
+        logoutUrl: process.env.SAML_ENTRY_POINT || 'https://vncidp.dev.vnc.de/saml2/idp/SingleLogoutService.php',
+        issuer: 'saml-localhost-dev',
+        acceptedClockSkewMs: -1,
+        cert: process.env.SAML_CERT || 'MIIEOzCCAyOgAwIBAgIJAIIxIoAmkptKMA0GCSqGSIb3DQEBBQUAMIGzMQswCQYDVQQGEwJDSDELMAkGA1UECAwCWkcxDDAKBgNVBAcMA1p1ZzEoMCYGA1UECgwfVk5DLSBWaXJ0dWFsIE5ldHdvcmsgQ29uc3VsdCBBRzEYMBYGA1UECwwPUmVkbWluZSBEZXYgSWRQMR4wHAYDVQQDDBVyZWRtaW5lZGV2aWRwLnZuYy5iaXoxJTAjBgkqhkiG9w0BCQEWFnN0ZWZhbi5zYWVuZ2VyQHZuYy5iaXowHhcNMTUxMjAxMTExMjA2WhcNMjUxMTMwMTExMjA2WjCBszELMAkGA1UEBhMCQ0gxCzAJBgNVBAgMAlpHMQwwCgYDVQQHDANadWcxKDAmBgNVBAoMH1ZOQy0gVmlydHVhbCBOZXR3b3JrIENvbnN1bHQgQUcxGDAWBgNVBAsMD1JlZG1pbmUgRGV2IElkUDEeMBwGA1UEAwwVcmVkbWluZWRldmlkcC52bmMuYml6MSUwIwYJKoZIhvcNAQkBFhZzdGVmYW4uc2FlbmdlckB2bmMuYml6MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwJ08bG2XuLqOURqjyUQYDqN/RiiVcE56CZi89jAaai9qDgvfyVPuimdgCAY18Rvh6dvf++0vTu1i4L6sElMvihnkD0+qm4alkQCOt5q/vEv7jvHG0PFbt2lQ6dGQm3OP6JfGatKZ6+UatuQScTQ08v6JR0S7VTElk3F7SYch9yyUAMM1pKfvakOnpy3XTZE8M9aABl5Ly1Xf3X9kxO3O2wqpKhyb6bUMN7UUe9p5SZhPx35i6UVMpekCtSo7pjIvGwQEnRxJEr2P4/RBNa7gDQtT6yLYa/MOhc3M8yViQyL/ASyJj9wcDVIurRTGXOuo9QQx8t8FX4vKjLiAyCxZsQIDAQABo1AwTjAdBgNVHQ4EFgQUvk2ifwnRhtiVedAi4wZ6AQEFa+EwHwYDVR0jBBgwFoAUvk2ifwnRhtiVedAi4wZ6AQEFa+EwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOCAQEAEha+N2LrJ1jsM12/ziPtorPGeJRbL7oALivq5odX3+1NGJ4htWc/zrxgsmKBml8E5QlnOWKLzixFs97GqA1P6SJWrG/HUXpKeZfIbFWecJVEqAW/+j6bgxFLtfbI0hQhKVD7vgJyulfmQofdBWcU6DQeClxG9/3ufNDmSGP7AUguzfp1T//ZupL54uWxHyzvmPvGSXGdHxv/8GEWEaH6vC/NJ70UORuQDjliQESgXN7Z+IGMmBYaFrONKuqbQ/CZiKuTO2vFs5kj50AZZsOKn9aP49m7wfqH0KJT33JY32lU/ntscFggvObSnNtzMvR91ZkGFhg4pvfu30vtH+Ltfg=='
       }
     }
   }
--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,16 @@
+const {XMLParser, XMLBuilder, XMLValidator} = require('fast-xml-parser');
+var uuidv1 = require('uuid/v1');
+var moment = require('moment');
+
+const xmloptions = {
+    ignoreAttributes : false,
+    attributeNamePrefix : "",
+    textNodeName: "$t"
+};
+
+const parser = new XMLParser(xmloptions);
+const builder = new XMLBuilder(xmloptions);
+
 
 module.exports = function (app, config, passport) {
 
@@ -50,10 +63,69 @@ module.exports = function (app, config, passport) {
   });
 
   app.get('/logout', function (req, res) {
-    req.logout();
-    // TODO: invalidate session on IP
 
-    res.redirect('/');
+    var samlReqId = "";
+    var samlResponseId = "_" + uuidv1().replace(/-/g, "");
+
+    var samlResponseTxt = "";
+    var issueInstant = moment().toISOString();
+    var newURI = "";
+    if (req.query && (req.query !== "" )) {
+      console.log("logout", req.query );
+
+      if (req.query.SAMLRequest && (req.query.SAMLRequest !== "")) {
+        var b64r1 = urldecode(req.query.SAMLRequest);
+        var b64buff = Buffer.from(b64r1, 'base64');
+        var samlreq = Zlib.inflateRawSync(b64buff);
+        var samlp = parser.parse(samlreq.toString());
+        samlReqId = samlp["samlp:LogoutRequest"].ID;
+        // logging("logout samlreq: ", samlreq.toString());
+        // logging("logout", samlp);
+        const logoutReqData = samlp["samlp:LogoutRequest"];
+        const logoutReqDataNameID = logoutReqData["saml:NameID"];
+        const logoutReqDataNameIDVal = logoutReqDataNameID["$t"];
+
+        var samlResponse = { 'samlp:LogoutResponse':
+            { 'xmlns:samlp': 'urn:oasis:names:tc:SAML:2.0:protocol',
+              'xmlns:saml': 'urn:oasis:names:tc:SAML:2.0:assertion',
+              ID: samlResponseId,
+              Version: '2.0',
+              'saml:Issuer':
+                { 'xmlns:saml': 'urn:oasis:names:tc:SAML:2.0:assertion',
+                  '$t': config.passport.saml.issuer },
+              IssueInstant: issueInstant,
+              Destination: config.passport.saml.logoutUrl,
+              InResponseTo: samlp["samlp:LogoutRequest"].ID,
+              'samlp:Status':
+                { 'samlp:StatusCode': { Value: 'urn:oasis:names:tc:SAML:2.0:status:Success' }
+                }
+            }
+        };
+
+
+        var fxpLogoutXML = builder.build(samlResponse);
+        var LogoutXMLdeflate = Zlib.deflateRawSync(Buffer.from(fxpLogoutXML));
+        var LogoutB64 = LogoutXMLdeflate.toString('base64');
+
+        newURI = config.passport.saml.logoutUrl + "?SAMLResponse=" + encodeURIComponent(LogoutB64);
+      }
+      if (req.query.RelayState && (req.query.RelayState !== "")) {
+        console.log("RelayState ", req.query.RelayState);
+        if (newURI !== "") {
+          newURI += "&RelayState=" + req.query.RelayState;
+        }
+      }
+    }
+
+
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      if (newURI == "") {
+        res.redirect('/login');
+      } else {
+        res.redirect(newURI);
+      }
+    });
   });
 
   app.get('/call-logout', passport.logoutSaml);
