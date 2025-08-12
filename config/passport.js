76e8bf849c866ada51a7577065ca404830fcc717
Stefan Saenger <stefan.saenger@vnc.biz>
2025-08-12 09:54:02 +0200

    fix: upgrade to @node-saml/passport-saml

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,4 @@
-const SamlStrategy = require('passport-saml').Strategy;
+const SamlStrategy = require('@node-saml/passport-saml').Strategy;
 
 module.exports = function (passport, config) {
 
@@ -6,22 +6,25 @@ module.exports = function (passport, config) {
   var samlStrategy = new SamlStrategy(
   {
     path: config.passport.saml.path,
+    callbackUrl: config.passport.saml.path,
     entryPoint: config.passport.saml.entryPoint,
     logoutUrl: config.passport.saml.logoutUrl,
     logoutCallbackUrl: config.passport.saml.logoutCallbackUrl,
     issuer: config.passport.saml.issuer,
+    idpCert: config.passport.saml.cert,
     cert: config.passport.saml.cert
   },
   function (profile, done) {
     saml.nameID = profile.nameID;
     saml.nameIDFormat = profile.nameIDFormat;
+    console.log("profile: ", profile);
     return done(null,
       {
         id: profile.uid,
         RedmineApiKey: profile.RedmineApiKey,
         email: profile.email,
-        displayName: profile.cn,
-        firstName: profile.givenName,
+        displayName: !!profile.cn ? profile.cn : profile.givenname + " " + profile.sn,
+        firstName: profile.givenname,
         lastName: profile.sn
       });
   });
